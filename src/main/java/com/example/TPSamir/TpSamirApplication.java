c690d61c3918873bd14bd9865384b627be9eb1a8
Samsam7504 <samir.yagoubi@ecoles-epsi.net>
2024-06-05 16:16:10 +0200

    Votre message de commit

---
+++ src/main/java/com/example/TPSamir/TpSamirApplication.java
@@ -0,0 +1,13 @@
+package com.example.TPSamir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSamirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSamirApplication.class, args);
+	}
+
+}
